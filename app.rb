fe4cb8bcd8c347a6195ccf6c7b6f0481841e9e1a
Ryan VonDohlen <ryan@ryanvondohlen.com>
2012-03-24 17:55:50 -0400

    wired up ohm

--- app.rb
+++ app.rb
@@ -15,9 +15,67 @@ get '/event/:user_id' do
   erb :index 
 end
 
-
+#
+# {project_name:"",project_path:"",file_path:"" }
+#
+#
+#
 
 post '/message/:user_id' do
+  Ohm.connect
+  
+  #find user
+  user_id=params[:user_id]
+  user = User.find(:email => user_id).all[0]
+  if user.nil?
+  	user = User.create(:email=>user_id)
+  end
+  
+  #find project
+  project_name = params[:project_name]
+  project = user.projects.find(:name => project_name).all[0]
+  if project.nil?
+  	project = Project.create(:name=>project_name)
+  	project.path=params[:project_path]
+  	project.user=user
+  	project.save
+  end
+  
+  event=Event.new
+  event.path = params[:file_path]
+  event.project = project
+  event.date = Time.now.strftime('%Y-%jT%T%:z')
+  event.save
   #recieve messages from watcher
   #save to db
 end
+
+class Event < Ohm::Model
+  attribute :path
+  attribute :date
+
+
+  index :path
+  index :date
+  reference :project, Project
+  
+end
+class Project < Ohm::Model
+  attribute :name
+  attribute :path
+  collection :events, Event
+
+  
+  index :name
+  index :path
+  reference :user, User
+end
+class User < Ohm::Model
+  attribute :email
+  collection :projects, Project
+
+  
+  index :email
+end
+
+
