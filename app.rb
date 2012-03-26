9b726e4ce4241486a22ed23b77a500b53b9c34dc
Kyle Fritz <kyle.p.fritz@gmail.com>
2012-03-25 22:41:44 -0400

    can always connect with url to ohm; include year

--- app.rb
+++ app.rb
@@ -6,47 +6,33 @@ set :static, true
 set :public_folder, "#{File.dirname(__FILE__)}/public"
 
 before do
- if ENV['REDISTOGO_URL']
    Ohm.connect :url => ENV['REDISTOGO_URL']
- else
-   Ohm.connect
- end
 end
-
 
 get '/' do
   @users = User.all.all
-  #server list of users
-  erb :index 
+  erb :index
 end
 
 
 get '/events/:user_id' do
   @user=User.find(:email =>params[:user_id]).all[0]
-  
-  if @user.nil?
-  	return 404
-  end
-  
-  
-  #events for this user
-  erb :events 
+
+  halt(404) if @user.nil?
+
+  erb :events
 end
 
-#
 # {project_name:"",project_path:"",file_path:"" }
-#
-#
-#
 
-post '/message/:user_id' do  
+post '/message/:user_id' do
   #find user
   user_id=params[:user_id]
   user = User.find(:email => user_id).all[0]
   if user.nil?
   	user = User.create(:email=>user_id)
   end
-  
+
   #find project
   project_name = params[:project_name]
   project = user.projects.find(:name => project_name).all[0]
@@ -56,14 +42,12 @@ post '/message/:user_id' do
   	project.user=user
   	project.save
   end
-  
+
   event=Event.new
   event.path = params[:file_path]
   event.project = project
   event.date = Time.now.getlocal
   event.save
-  #recieve messages from watcher
-  #save to db
 end
 
 class Event < Ohm::Model
@@ -74,6 +58,7 @@ class Event < Ohm::Model
   index :path
   index :date
   reference :project, Project
+
   def to_hash
     super.merge(:path => path,:date => date)
   end
@@ -83,11 +68,11 @@ class Project < Ohm::Model
   attribute :path
   collection :events, Event
 
-  
+
   index :name
   index :path
   reference :user, User
-  
+
   def to_hash
     super.merge(:name => name,:path => path, :events => events.all)
   end
@@ -96,12 +81,10 @@ class User < Ohm::Model
   attribute :email
   collection :projects, Project
 
-  
+
   index :email
-  
+
   def to_hash
     super.merge(:email => email, :projects => projects.all)
   end
 end
-
-
